cba7813188e6501d712d46af68784d1d79c4f955
JackFogarty02 <jackfogarty2002@gmail.com>
2022-12-06 14:27:47 +1100

    Got keyboard flight controls working
    Now to get it working with a joystick

--- SingleInputJam/Assets/Scripts/SpaceshipMovement.cs
+++ SingleInputJam/Assets/Scripts/SpaceshipMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpaceshipMovement : MonoBehaviour
 {
-    public bool throttle => Input.GetKey(KeyCode.W);
+    public bool throttle => Input.GetKey(KeyCode.Space);
     public bool rise => Input.GetKey(KeyCode.UpArrow);
     public GameObject spaceShip;
 
@@ -22,13 +22,29 @@ public class SpaceshipMovement : MonoBehaviour
     {
         if (throttle)
         {
-            transform.position += transform.forward * enginePower * Time.deltaTime;
-        }
+            transform.position += transform.forward * enginePower * Time.deltaTime; //Pushed the ship forward
 
-        if (rise)
+            activePitch = Input.GetAxisRaw("Vertical") * pitchPower * Time.deltaTime; //Asigns the active variables
+            activeRoll = Input.GetAxisRaw("Horizontal") * rollPower * Time.deltaTime;
+            activeYaw = Input.GetAxisRaw("Yaw") * yawPower * Time.deltaTime;
+
+            transform.Rotate(activePitch * pitchPower * Time.deltaTime, 
+                            activeYaw * yawPower * Time.deltaTime, 
+                            activeRoll * rollPower * Time.deltaTime, Space.Self); //Transforms the objects rotation when the assigned inputs are pressed
+        }
+        else
         {
+            transform.position += transform.forward * enginePower * Time.deltaTime; //Pushed the ship forward
 
-            //transform.rotation += pitchPower * Time.deltaTime;
+            activePitch = Input.GetAxisRaw("Vertical") * pitchPower/2 * Time.deltaTime; //Asigns the active variables
+            activeRoll = Input.GetAxisRaw("Horizontal") * rollPower/2 * Time.deltaTime;
+            activeYaw = Input.GetAxisRaw("Yaw") * yawPower/2 * Time.deltaTime;
+
+            transform.Rotate(activePitch * pitchPower * Time.deltaTime,
+                            activeYaw * yawPower * Time.deltaTime,
+                            activeRoll * rollPower * Time.deltaTime, Space.Self); //Transforms the objects rotation when the assigned inputs are pressed
         }
+
+
     }
 }
