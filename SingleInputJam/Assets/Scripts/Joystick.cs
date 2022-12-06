e887417ea31aee1a0a9912bff2c97d077c86314d
JackFogarty02 <jackfogarty2002@gmail.com>
2022-12-06 15:51:06 +1100

    Some progress in joystick

--- SingleInputJam/Assets/Scripts/Joystick.cs
+++ SingleInputJam/Assets/Scripts/Joystick.cs
@@ -8,6 +8,9 @@ public class Joystick : MonoBehaviour
     private Vector2 pointA;
     private Vector2 pointB;
 
+    public Transform circle;
+    public Transform outerCircle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,14 +20,15 @@ public class Joystick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*if (Input.touchCount == 1)
-        {
-            pointA = Input.Camera.main.ScreenToWorldPoint;
-        }*/
 
         if (Input.GetMouseButtonDown(0))
         {
             pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+
+            circle.position = pointA * -1;
+            outerCircle.position = pointA * -1;
+            circle.GetComponent<SpriteRenderer>().enabled = true;
+            outerCircle.GetComponent<SpriteRenderer>().enabled = true;
         }
         if (Input.GetMouseButtonDown(0))
         {
@@ -33,7 +37,7 @@ public class Joystick : MonoBehaviour
         }
         else
         {
-            touchStart = false;
+            touchStart = false;            
         }
     }
     private void FixedUpdate()
@@ -42,6 +46,15 @@ public class Joystick : MonoBehaviour
         {
             Vector2 offset = pointB - pointA;
             Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+
+            circle.transform.position = new Vector2(pointA.x + direction.x, pointA.y + direction.y) * -1;
+
+            Debug.Log(direction);
+        }
+        else
+        {
+            circle.GetComponent<SpriteRenderer>().enabled = false;
+            outerCircle.GetComponent<SpriteRenderer>().enabled = false;
         }
     }
 }
